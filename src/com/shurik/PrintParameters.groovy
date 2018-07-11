bd574110e8b20836f914a139a716f1dc715d8b5d
shurikg <shurikgefter@gmail.com>
2018-07-11 16:51:22 +0300

    new print class

---
+++ src/com/shurik/PrintParameters.groovy
@@ -0,0 +1,17 @@
+package com.shurik
+
+class PrintParameters implements Serializable {
+    def script
+    def stepName
+    
+    PrintParameters(script, String stepName) {
+        this.script = script
+        this.stepName = stepName
+    }
+    
+    def logParameters( Map parameters ){
+        def message = "[DEBUG][${this.stepName}] Method parameters:\n"
+        parameters.each{ k, v -> message = message + "[DEBUG][${this.stepName}]      ${k}=[${v}] \n" }
+        this.script.echo message
+    }
+}
